657de806f6a26549d44c33ffdcfa4fd54590748e
Praveen Kumar A <55251825+praveenadoni4456@users.noreply.github.com>
2023-03-13 13:05:53 +0530

    Update outputs.tf

--- 20-shared-vpc-networking/modules/networking/outputs.tf
+++ 20-shared-vpc-networking/modules/networking/outputs.tf
@@ -2,8 +2,9 @@ output "vpc" {
   description = "Shared VPCs information"
   value       = module.vpc
 }
-
+/*
 output "vpc_service_perimeter" {
   description = "VPC Service Permiter"
   value       = module.regular_service_perimeter
 }
+*/
